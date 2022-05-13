cf9bf6de2513ee64a2080ad31d25dc63488d813a
Patrick Felschen <patrick@felschen.de>
2022-05-13 11:33:22 +0200

    user interface

---
+++ Blatt06/Server/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    void printHelp(){
+        System.out.println("Server:");
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.printHelp();
+    }
+}
