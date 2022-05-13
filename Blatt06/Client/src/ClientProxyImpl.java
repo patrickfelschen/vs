24b8d066c2e3e1082cb951f9f1c8b3c2f3adc37c
Julian Voss <julian.voss@hs-osnabruecl.de>
2022-05-13 11:06:16 +0200

    Interface Implementationen

---
+++ Blatt06/Client/src/ClientProxyImpl.java
@@ -0,0 +1,8 @@
+import java.rmi.RemoteException;
+
+public class ClientProxyImpl implements ClientProxy{
+    @Override
+    public void receiveMessage(String username, String message) throws RemoteException {
+
+    }
+}
---
+++ Blatt06/Server/src/ChatServerImpl.java
@@ -0,0 +1,8 @@
+import java.rmi.RemoteException;
+
+public class ChatServerImpl implements ChatServer {
+    @Override
+    public void sendMessage(String message) throws RemoteException {
+
+    }
+}
